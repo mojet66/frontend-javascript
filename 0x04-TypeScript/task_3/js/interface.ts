72c9d931aba1f3a383f7b145420a74b5308160a7
mojet66 <oladosuemmanuel2017@gmail.com>
2025-05-25 11:36:32 +0100

    finishing task_4

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define RowID as a number
+export type RowID = number;
+
+// Define the structure of a row element
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // Optional
+}
